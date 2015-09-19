6a420f6c62960612cd9d730a063b0939ecae4360
Ahmad Bushnaq <ahmad.bushnaq@weather.com>
2015-09-19 07:20:41 -0400

    Initial Commit

---
+++ StuffAndThings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StuffAndThings
+//
+//  Created by Ahmad Bushnaq on 9/19/15.
+//  Copyright © 2015 Ahmad Bushnaq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
